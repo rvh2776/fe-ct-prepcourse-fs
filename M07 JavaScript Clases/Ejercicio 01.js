d007c6ce8714be7f6571444e309f4b543ae72f7d
Rafael V.H <rafael.vh@gmail.com>
2023-11-02 14:03:43 -0300

    M07 javascript Clases terminado

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,79 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+
+   class Usuario {
+
+      constructor(usuario, nombre, email, password) {
+         
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+
+      saludar() {
+         return `Hola, mi nombre es ${this.nombre}`;
+      }
+   }
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+
+   //? Agregar metodo al prototipo con funcion tradicional
+ 
+   // Usuario.prototype.saludar = function() {
+   //    return 'Hello World!';
+   // }
+
+   //? Agregar metodo al prototipo con arrow function.
+
+   Usuario.prototype.saludar = () => {
+      return 'Hello World!';
+   }
+
+   return Usuario;
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+
+   //? devolviendo string invertido usando function arrow
+   // String.prototype.reverse = function() {
+
+   //    const reverseStr = (str) => str.split('').reverse().join('');
+
+   //    const texto = reverseStr(this);
+
+   //    return texto;
+   // }
+
+   // //? devolviendo string invertido usando split, reverse y join separados. 
+   // String.prototype.reverse = function() {
+
+   //    let str = this;
+
+   //    separo = str.split('')
+   //    invierto = separo.reverse()
+   //    texto = invierto.join('');
+ 
+   //    return texto
+   // }
+
+   String.prototype.reverse = function() {
+
+      texto = this.split('').reverse().join('');
+
+      return texto;
+   }
+
+   return String;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
